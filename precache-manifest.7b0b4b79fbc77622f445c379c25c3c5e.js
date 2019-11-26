461e4c2e2ecb41141c1a8ec7db94e6beb16a162c
Eduardo Santos <e3duardo@live.com>
2019-11-26 20:49:44 +0000

    [skip ci] Update site

---
+++ precache-manifest.7b0b4b79fbc77622f445c379c25c3c5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f825243941a6afc14bd09721a09f67b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49eef2887eb7ba6e9973",
+    "url": "/static/css/2.07b170e0.chunk.css"
+  },
+  {
+    "revision": "49eef2887eb7ba6e9973",
+    "url": "/static/js/2.595e41e7.chunk.js"
+  },
+  {
+    "revision": "c1e01519bd8914759670",
+    "url": "/static/js/main.4f7a7912.chunk.js"
+  },
+  {
+    "revision": "68aaf562a15bed2debfc",
+    "url": "/static/js/runtime-main.59536966.js"
+  }
+]);
