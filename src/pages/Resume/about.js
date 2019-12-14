cffbb7567c68711dd8c8138215a71226f7805e93
Eduardo Santos <e3duardo@live.com>
2019-12-14 13:57:16 -0300

    atualizando ortografia

--- src/pages/Resume/about.js
+++ src/pages/Resume/about.js
@@ -9,16 +9,33 @@ export default function About() {
         <Tag tag="h3">About me</Tag>
       </h3>
       <p>
-        Graduating in Information Systems, I seek opportunity to share learning
-        and learn.
+        I am a full-stack developer with extensive experience in web
+        development, always looking to update myself with new technologies and
+        improve the knowledge I already have. I currently live in Brazil with my
+        wife and pet, looking for an opportunity to show the best of my skills.
       </p>
       <p>
-        As a Systems Analyst, I have 4 years developing Java and web
-        technologies.
+        My strength relies on web development. I was fortunate to work with some
+        very experienced senior developers in my workplace. My focus till now
+        has been Java and Java EE development, but I am passionate about the
+        front-end, and I'm looking to improve into React and its ecosystem. I
+        always think of the quality of the code when I am developing.
       </p>
       <p>
-        Knowledge in Java and Hibernate. Special attention to React, website
-        optimization and passion for the learning process.
+        I'm looking to be a recognised professional in the market as a
+        reference. I have a good experienced with front-end and React, but I'm
+        open with other technologies. For now, I'm seeking to prove my skills,
+        to soon become a technical leader.
+      </p>
+      <p>
+        I am a family person who loves my wife and my dog, always in touch with
+        proper technologies. When I'm not at work, I like to travel, develop my
+        projects and learn new things.
+      </p>
+      <p>
+        I love web development, and I like to research the whole environment
+        involved, like accessibility and website optimisation. Making slowest
+        websites into lightweight ones is something I love to do.
       </p>
     </div>
   );
--- src/pages/Resume/experiences.js
+++ src/pages/Resume/experiences.js
@@ -18,32 +18,34 @@ export default function Experi() {
         teaching basic computer
       </Experience>
       <Experience
-        company="centro universitário redentor"
+        company="Centro Universitário Redentor"
         season="2015 - today"
         job="Systems Analyst"
       >
-        I started here as an IT assistant and in 2016 I migrated to the
-        development area. As a systems analyst. I use Java and Java EE daily,
-        our projects are developed using VRaptor, JSP, Hibernate, MySql and for
-        frontend, we use Bootstrap, JQuery, and various plugins. Our main
-        project is the institution&apos;s portal, which uses the technologies
-        above, as well as other projects with the same stack.
+        I started in Centro Universitário Redentor as an IT assistant in 2015.
+        And in 2016, I migrated to the development area. As a Systems Analyst, I
+        use Java and Java EE daily, and I also use a Brazilian framework VRaptor
+        and JSP, Hibernate, MySQL. In the frontend, I generally use Bootstrap,
+        JQuery, and various plugins. Our main project is the institution's
+        portal. I also make part of the development team of a web system for the
+        small student hospital.
         <br />
         <br />
         In addition to growing a lot as a web developer here, I was able to
-        learn about DevOps, data modeling, and mobile application development
-        with PhoneGap.
+        learn about DevOps, CI, Data Modeling, and mobile application
+        development with PhoneGap.
       </Experience>
-      <Experience company="own" season="2008 - today" job="Freelancer">
-        I started freelancing in 2008 developing websites and systems using
-        technologies like PHP and C #. Later I continued with PHP (sometimes
+      <Experience company="Autonomous" season="2008 - today" job="Freelancer">
+        I started as a freelancer in 2008, developing websites and systems using
+        technologies like PHP and C#. Later, I continued with PHP (sometimes
         WordPress) and started using Ruby on Rails and Jekyll in some projects.
-        I am currently focused on React development.
+        By now, my focus is React development, generally in adjunct with NodeJs
+        API.
         <br />
         <br />
         In this walk, I learned a lot about website creation, website
-        optimization, continuous integration and everything needed to create on
-        the web easily and quickly.
+        optimization, continuous integration and everything needed to create
+        excellent websites easily and quickly.
       </Experience>
     </div>
   );
